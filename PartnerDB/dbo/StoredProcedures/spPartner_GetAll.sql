cb51ca73e08ba218961daaa4bd9d3e0f90823c93
Ranko1996 <koturic96@gmail.com>
2025-04-24 20:18:19 +0200

    Add project files.

---
+++ PartnerDB/dbo/StoredProcedures/spPartner_GetAll.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spPartner_GetAll]
+AS
+begin
+	select
+		FirstName,
+        LastName,
+        FirstName + ' ' + LastName AS FullName,
+		PartnerNumber,
+        CroatianPIN,
+        PartnerTypeId,
+        CreatedAtUtc,
+        IsForeign,
+        Gender
+	from dbo.[Partner];
+end
---
+++ PartnerDB/dbo/Tables/Partner.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Partner]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(255) NOT NULL CHECK (LEN(FirstName) >= 2),
+    [LastName] NVARCHAR(255) NOT NULL CHECK (LEN(LastName) >= 2),
+    [Address] NVARCHAR(255) NULL,
+    [PartnerNumber] CHAR(20) NOT NULL CHECK (PartnerNumber NOT LIKE '%[^0-9]%'), -- točno 20 znamenki
+    [CroatianPIN] CHAR(11) NULL CHECK (CroatianPIN LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'), -- OIB ima 11 znamenki
+    [PartnerTypeId] INT NOT NULL CHECK (PartnerTypeId IN (1, 2)),
+    [CreatedAtUtc] DATETIME NOT NULL DEFAULT(GETUTCDATE()),
+    [CreatedByUser] NVARCHAR(255) NOT NULL CHECK (CreatedByUser LIKE '_%@_%._%'), -- gruba provjera maila
+    [IsForeign] BIT NOT NULL,
+    [ExternalCode] NVARCHAR(20) NOT NULL UNIQUE CHECK (LEN(ExternalCode) BETWEEN 10 AND 20),
+    [Gender] CHAR(1) NOT NULL CHECK (Gender IN ('M', 'F', 'N'))
+)
